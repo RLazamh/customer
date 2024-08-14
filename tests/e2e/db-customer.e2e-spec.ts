b99d0806d3b75f41c06679e5356eedf2b0ce6977
RLazamh <roger.laza@epn.edu.ec>
2024-08-13 23:03:10 -0500

    add test config for e2e and unit

--- tests/e2e/db-customer.e2e-spec.ts
+++ tests/e2e/db-customer.e2e-spec.ts
@@ -1,12 +1,12 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
-import { AppModule } from '../src/app.module';
+import { AppModule } from '.../../../src/app.module';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
@@ -15,10 +15,15 @@ describe('AppController (e2e)', () => {
     await app.init();
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+  afterAll(async () => {
+    await app.close();
+  });
+
+  it('createCustomer', async () => {
+    const customerId = 'e9a4a697-a47c-4ddf-9d46-7527161e74be';
+    const customerResponse = await request(app.getHttpServer())
+      .get(`/customer/${customerId}`)
+      .expect(200);
+    console.log(customerResponse.body);
   });
 });
