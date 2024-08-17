0a4b65e86cdb514f60e6d3433a3aea9ebcf50275
RLazamh <roger.laza@epn.edu.ec>
2024-08-17 06:57:17 -0500

    add test for domain layer

--- src/domain/customer/customer-rules.dom.service.ts
+++ src/domain/customer/customer-rules.dom.service.ts
@@ -1,10 +1,11 @@
 import { Injectable } from '@nestjs/common';
-import { ERROR_STATUS_CODE } from '../..//application/utils';
+import { ERROR_STATUS_CODE } from '../../application/utils';
+import { PHONE_PREFIX } from '../constants.dom';
 
 @Injectable()
 export class CustomerRulesDomService {
-  validatePhoneNumber(phone: string) {
-    if (phone.slice(0, 2) !== '09') {
+  validatePhoneNumber(phone: string): void {
+    if (phone.slice(0, 2) !== PHONE_PREFIX) {
       throw ERROR_STATUS_CODE.INVALIDATE_PHONE_NUMER;
     }
   }
--- src/domain/index.ts
+++ src/domain/index.ts
@@ -1,2 +1,3 @@
 export * from './customer/customer-rules.dom.service';
 export * from './repositories/db.repository';
+export * from './constants.dom';
